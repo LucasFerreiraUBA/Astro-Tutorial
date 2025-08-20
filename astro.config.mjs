3b79aa5903559550824645fce2c7b68524d5d6ae
Lucas Ferreira <lferreira@fi.uba.ar>
2025-08-20 18:12:52 -0300

    ADD: Adding some interactive islands, dark and lighte theme done!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://lferreira-astro.netlify.app/"
+  site: "https://lferreira-astro.netlify.app/",
+  integrations: [preact()]
 });
